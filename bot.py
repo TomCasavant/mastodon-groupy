092c0856f0237dacf7c8b9f6b9294d488baa631f
Tom Casavant <tfcasavant@gmail.com>
2024-02-02 13:12:33 -0500

    Fixed searching for profile, added update_profile() with config fields to fix #2

--- bot.py
+++ bot.py
@@ -23,27 +23,53 @@ class Bot:
   def is_post(self, status) -> bool:
     return status.get('reblog') and '/post/' in status.get('reblog').get('url')
 
+  '''
+    Loops through all communities from config, attempts to search for and follow account
+  '''
   def follow_communities(self):
     users_to_follow = self.bot_config.get('communities')
     for user in users_to_follow:
       # Get account ID from username
       print(f"Attempting to follow {user}")
-      account = self.mastodon.account_search(user)[0]
+      account = self.mastodon.account_search(user, resolve=True)[0]
       account_id = account['id']
+      print(account_id)
       # Follow the user
-      self.mastodon.account_follow(account)
+      print(self.mastodon.account_follow(account))
 
+  """
+    If status is a post, boost it. Otherwise ignore it
+  """
   def on_update(self, status):
     if (not status['reblogged'] and self.is_post(status)):
       print("Boosting status")
       print(status)
       self.mastodon.status_reblog(status['id'])
 
+  '''
+    Creates a stream and attaches it to on_update
+  '''
   def stream_timeline(self):
+    print("Starting stream")
     listener = CallbackStreamListener(update_handler = self.on_update)
     self.mastodon.stream_user(listener, timeout=1200, reconnect_async=True)
 
+  '''
+    Uses config to update profile with display name, description, and profile fields
+  '''
+  def update_profile(self):
+    display_name = self.mastodon_config.get('display_name')
+    note_preface = self.mastodon_config.get('bio_preface')
+    note_appendix = self.mastodon_config.get('bio_appendix')
+    communities = '\n'.join(self.bot_config.get('communities'))
+    note = f"{note_preface}\n{communities}\n\n{note_appendix}"
+    print(self.mastodon_config)
+    fields = [(field['descriptor'], field['value']) for field in self.mastodon_config.get('profile_field')]
+    print(display_name, note, fields)
+    self.mastodon.account_update_credentials(display_name=display_name, note=note, bot=True, fields=fields)
+
 if __name__ == "__main__":
   bot = Bot()
+  bot.update_profile()
   bot.follow_communities()
   bot.stream_timeline()
