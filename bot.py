2a8f4322b9251a744e10b92859ea982ae9ff80e9
Tom Casavant <tfcasavant@gmail.com>
2024-02-02 10:12:57 -0500

    Change how groupy determines if a status is a post or comment, should fix #5

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ class Bot:
       self.mastodon = self.login()
 
   def login(self):
-    #mastodon_config = self.config.get('mastodon')
     server = self.mastodon_config.get('server')
     print(f"Logging into {server}")
     return Mastodon(access_token=self.mastodon_config.get('access_token'), api_base_url=server)
@@ -22,20 +21,9 @@ class Bot:
      Returns true if status is top level (a post), false otherwise (a comment)
   '''
   def is_post(self, status) -> bool:
-    print(status.get('in_reply_to_id'))
+    return status.get('reblog') and '/post/' in status.get('reblog').get('url')
 
-    return status.get('reblog') and not status.get('reblog').get('in_reply_to_id')
-
-#  def follow_config(self) -> None:
-#    communities = self.bot_config.get('communities')
-#    for community in communities:
-#      print(f"Following {community}")
-#      self.mastodon.status_post(f"Following {community}")
-      #account_id = self.mastodon.account_search(community)[0]['id']
-      #print(f"Found {account_id}")
-#      self.mastodon.account_follow(community)
-  def follow_config(self):
-    #mastodon = self.login()
+  def follow_communities(self):
     users_to_follow = self.bot_config.get('communities')
     for user in users_to_follow:
       # Get account ID from username
@@ -43,62 +31,19 @@ class Bot:
       account = self.mastodon.account_search(user)[0]
       account_id = account['id']
       # Follow the user
-      self.mastodon.account_follow(account_id)
-
-  #def has_boosted_status(self, status_id):
-      #mastodon = self.login()
-   #   reblogged_by = self.mastodon.status_reblogged_by(status_id)
-    #  print(reblogged_by)
-     # authenticated_account_id = self.mastodon.account_verify_credentials()['id']
-     # return authenticated_account_id in reblogged_by
-
-
-  '''
-    Loops through timeline and boosts all top-level posts, then sleeps
-  '''
-  def stream_timelines(self):
-    # Follow all accounts from config
-    self.follow_config()
-    latest_status_id = None
-    while True:
-      all_statuses = []
-      if (not latest_status_id):
-        statuses = self.mastodon.timeline_home()
-      else:
-        satuses = self.mastodon.timeline_home(since_id=latest_status_id)
-
-      all_statuses.extend(statuses)
-      while self.mastodon.timeline_home(since_id=latest_status_id, max_id=statuses[-1]['id']):
-         statuses = self.mastodon.timeline_home(since_id=latest_status_id, max_id=statuses[-1]['id'])
-         all_statuses.extend(statuses)
-      for status in all_statuses:
-        if (not status['reblogged'] and self.is_post(status)):
-          print(f"Boosting status {status}")
-          self.mastodon.status_reblog(status['id'])
-        else:
-          print("Not boostable")
-      if all_statuses:
-        latest_status_id = all_statuses[0]['id']
-
-      time.sleep(60*10)
-
+      self.mastodon.account_follow(account)
 
   def on_update(self, status):
     if (not status['reblogged'] and self.is_post(status)):
       print("Boosting status")
+      print(status)
       self.mastodon.status_reblog(status['id'])
-    else:
-      print ("Not boostable")
 
   def stream_timeline(self):
-#   print(self.mastodon.retrieve_mastodon_version())
-#    print(self.mastodon._Mastodon__instance())
     listener = CallbackStreamListener(update_handler = self.on_update)
-    #print(self.mastodon.retrieve_mastodon_version())
-    #self.mastodon.stream_user( { 'update': self.on_update }, run_async=False)
     self.mastodon.stream_user(listener, timeout=1200, reconnect_async=True)
 
 if __name__ == "__main__":
   bot = Bot()
-  bot.follow_config()
+  bot.follow_communities()
   bot.stream_timeline()
