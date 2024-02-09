bd635ab2d1263eb8b4482ab744dbf52d5a6d0e20
Tom Casavant <tfcasavant@gmail.com>
2024-02-09 14:20:29 -0500

    Fixed contains issue

--- bot.py
+++ bot.py
@@ -33,8 +33,8 @@ class Bot:
     url = status.get('reblog').get('url')
     if '/comment/' in url or '#entry-comment-' in url:
       return False
-    if not url.contains('post'):
-      if not url.contains('/t/'):
+    if not 'post' not in url:
+      if not '/t/' not in url:
         return False
     return True
 
